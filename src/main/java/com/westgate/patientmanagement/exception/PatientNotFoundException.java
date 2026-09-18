73c51e9fa678d3cb42a103ce85d5ebb88546fe5a
Gundeboina Krishna <sivakrishnagundeboina@gmail.com>
2026-09-18 10:32:11 +0530

    Delete src directory

--- src/main/java/com/westgate/patientmanagement/exception/PatientNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.westgate.patientmanagement.exception;
-
-public class PatientNotFoundException extends RuntimeException {
-
-	public PatientNotFoundException(String message) {
-		super(message);
-	}
-}
